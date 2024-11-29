4a834dd60fe366942146db4d53403ec18ace058e
bilalnessMaster <jijahgiga@gmail.com>
2024-11-29 12:13:26 +0100

    add animations for introduction section and logoticker

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,57 +1,109 @@
-'use client'
+"use client";
 import Button from "@/components/Button";
-import designExample1Image from '@/assets/images/design-example-1.png'
-import designExample2Image from '@/assets/images/design-example-2.png'
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
 import Pointer from "@/components/Point";
 import Image from "next/image";
-import {motion , useAnimate} from 'framer-motion';
+import { motion, useAnimate } from "framer-motion";
 import { useEffect, useRef } from "react";
+import cursorPointer from '@/assets/images/cursor-you.svg'
 export default function Hero() {
-    const containerRef = useRef(null)
-    const [leftDesignScope , LeftDesignAnimate ] = useAnimate();
-    const [leftPointerScope , LeftPointerAnimate ] = useAnimate();
-    useEffect(()=>{
-        LeftDesignAnimate([
-    [ leftDesignScope.current,{opacity : 1}, {duration : 0.5}], 
-    [ leftDesignScope.current,{y : 0 , x :0 }, {duration : 0.5}], 
-        ])
-        LeftPointerAnimate([
-            [leftPointerScope.current , {opacity : 1 }, {duration : 0.5}],
-            [leftPointerScope.current , {y: 0 , x : -100}, {duration : 0.5}], 
-            [leftPointerScope.current , {y: [0 , 16 , 0] , x : 0 }, {duration : 0.5 , ease : 'easeInOut'}]
-        ])
-    },[]);
-    return <section className="py-24 overflow-x-clip">
-        <div className="container relative ">
-            <motion.div ref={leftDesignScope} initial={{opacity : 0,y : 100 , x: -100}}className="absolute -left-32 top-16 hidden lg:block">
-                <Image src={designExample1Image} alt="example 1" />
-            </motion.div>
-            <motion.div initial={{opacity : 0 , y: 100 ,x : -200}} ref={leftPointerScope} className="absolute left-56 top-96 hidden lg:block">
-                <Pointer name="Andrea" />
-            </motion.div>
-            <div className="absolute -right-64 -top-16 hidden lg:block ">
-                <Image src={designExample2Image} alt="example 2" />
-            </div>
-            <div className="absolute right-80 -top-4 hidden lg:block">
-                <Pointer name="Bryan " color="red"/>
-            </div>
-            <div className="flex justify-center">
-                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold ">
-                    ✨ $7.5M seed round raised
+    const containerRef = useRef(null);
+    const [leftDesignScope, LeftDesignAnimate] = useAnimate();
+    const [leftPointerScope, LeftPointerAnimate] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
 
+    useEffect(() => {
+        LeftDesignAnimate([
+            [
+                leftDesignScope.current, 
+                { opacity: 1 }, { duration: 0.5 }],
+            [
+                leftDesignScope.current, 
+                { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+        LeftPointerAnimate([
+            [
+                leftPointerScope.current, 
+                { opacity: 1 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                 { y: 0, x: -100 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                { y: [0, 16, 0], x: 0 },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+        rightDesignAnimate([
+            [rightDesignScope.current , {opacity : 1} , {duration : 0.5 , delay: 1.5}],
+            [rightDesignScope.current , { y: 0, x:0 } , {duration : 0.5 }],
+        ])
+        rightPointerAnimate([
+            [rightPointerScope.current ,{opacity : 1 } , {duration : 0.5 , delay: 1.5}],
+            [rightPointerScope.current ,{opacity : 1 , x:200 , y : 100 } , {duration : 0.5}],
+            [rightPointerScope.current ,{opacity : 1 , x:[180,0] , y : 0 } , {duration : 0.5 , ease : 'easeInOut'}],
+        ])
+    }, []);
+    return (
+        <section className="py-24 overflow-x-clip" style={{
+            cursor :`url(${cursorPointer.src}) auto`
+        }}>
+            <div className="container relative ">
+                <motion.div drag
+                    ref={leftDesignScope}
+                    initial={{ opacity: 0, y: 100, x: -100 }}
+                    className="absolute -left-32 top-16 hidden lg:block"
+                >
+                    <Image draggable={false} src={designExample1Image} alt="example 1" />
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, y: 100, x: -200 }}
+                    ref={leftPointerScope}
+                    className="absolute left-56 top-96 hidden lg:block"
+                >
+                    <Pointer name="Andrea" />
+                </motion.div>
+                <motion.div drag initial={{opacity: 0 , x:100 , y:200 }} ref={rightDesignScope} className="absolute -right-64 -top-16 hidden lg:block ">
+                    <Image  draggable={false} src={designExample2Image} alt="example 2" />
+                </motion.div>
+            <motion.div ref={rightPointerScope} initial={{opacity : 0 , x:300 , y : 300}} className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Bryan " color="red" />
+                </motion.div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold ">
+                        ✨ $7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-medium text-center mt-6 ">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+                <div className="flex justify-center ">
+                    <form
+                        action=""
+                        className="max-w-lg w-full mx-auto flex border border-white/15 rounded-full p-2 mt-8 justify-between "
+                    >
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            className="bg-transparent sm:w-52 outline-none md:flex-1 px-4 "
+                        />
+                        <Button
+                            type="submit"
+                            variant="primary"
+                            className="whitespace-nowarp "
+                        >
+                            Sign Up
+                        </Button>
+                    </form>
                 </div>
             </div>
-            <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-medium text-center mt-6 ">Impactful design, created effortlessly</h1>
-            <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
-                Design tools shouldn&apos;t slow you down. Layers
-                combines powerful features with an intuitive interface that keeps you in your creative flow.
-            </p>
-            <div className="flex justify-center ">
-                <form action="" className="max-w-lg w-full mx-auto flex border border-white/15 rounded-full p-2 mt-8 justify-between ">
-                    <input type="email" placeholder='Enter your email' className="bg-transparent sm:w-52 outline-none md:flex-1 px-4 " />
-                    <Button type="submit" variant='primary' className="whitespace-nowarp "  >Sign Up</Button>
-                </form>
-            </div>
-        </div>
-    </section>;
+        </section>
+    );
 }
--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,12 +1,28 @@
+'use client'
 import Tag from "@/components/Tag";
-
+import {useMotionValueEvent, useScroll, useTransform} from 'framer-motion'
+import { useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
-
+const words = text.split(' ');
 export default function Introduction() {
+    const [currentWord , setCurrentWord] = useState<number>(0)
+    const refContainer = useRef<HTMLDivElement>(null)
+    const {scrollYProgress} = useScroll({
+        target : refContainer ,
+        offset : ['start end' , 'end end']
+    })
+    const wordindex = useTransform(scrollYProgress , [0,1] , [0,words.length])
+    useEffect(()=>{
+        wordindex.on('change' , (lastest)=>{
+            setCurrentWord(lastest)
+        })
+    },[wordindex])
     return (
         <section className="py-28 lg:py-40">    
           <div className="container 0 ">
        
+           <div className="sticky top-28">
            <div className="text-center">
            <Tag>
             Introducing Layers
@@ -18,13 +34,19 @@ export default function Introduction() {
                 <span>Your creative
                 process deserves better.
                 </span>{" "}
-                 <span className="text-white/15">
-                    {text}
+                 <span className="text-white/15 ">
+                    {words.map((word , index)=>(
+                        <span key={index} className={twMerge("transition text-white/15" , currentWord > index && "text-white ")}>{`${word} `}</span>
+                    ))}
                 </span>{" "}
                 <span className="text-lime-400">
                     That's why we built Layers.
                 </span>
             </p>
+           </div>
+           </div>
+           <div className="h-[150vh]" ref={refContainer}>
+
            </div>
             </div>
         
